9d392d2bd64fc72f284e5bfcaa5fc16569e5014c
Sabrina SAHLI <ninasitdev11@gmail.com>
2021-11-25 08:17:55 +0100

    header et footer style

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -1,3 +1,39 @@
+<footer class="footer">
+
+    <!-- <div class="footer-logo col-4">
+         <a href="?"><img src="images/logo.jpg" alt="Logo Project Hive"></a>
+    </div>  -->
+
+    <!-- <div class="footer-navigation col-10"> -->
+        <div class="nav about us">
+            <a href="">A propos de nous</a>
+        </div>
+        <div class="nav legal">
+            <a href="">Mentions légales</a>
+        </div>
+        <div class="nav GDRP">
+            <a href="">RGPD</a>
+        </div>
+    <!-- </div> -->
+
+    <div class="footer-favicons col-2">
+        <div class="facebook">
+            <i class="fab fa-facebook-square"></i>
+        </div>
+        <div class="instagram">
+            <!-- <i class="fa-brands fa-instagram"></i> j'ai mis l'autre logo pour être raccord avec les autres-->
+            <i class="fab fa-instagram-square"></i>
+        </div>
+        <div class="twitter">
+            <i class="fab fa-twitter-square"></i>
+        </div>
+        <div class="linkedin">
+            <i class="fab fa-linkedin"></i>
+        </div>
+    </div>
+</footer>
 </div>
+
 </body>
+
 </html>
