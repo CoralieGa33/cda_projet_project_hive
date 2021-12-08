aa3dc99a69475134d5a8ddaa3435f63624ca0add
ChloéR <chloe.rotaru@yahoo.com>
2021-12-08 12:17:38 +0100

    menuquimarche

--- app/includes/burger.php
+++ app/includes/burger.php
@@ -1,49 +1,55 @@
 
 <div class="menu-burger">
-           <div id="burger-wrap">
-                <header class="burger-header">
-                    <nav class="burger-nav">
-                        <a href="#burger-wrap" id="burger-open">
-                        <i class="fas fa-arrow-right"></i>
-                        </a>
-                            <a href="#" id="close"><i class="fas fa-arrow-left"></i></a>
-                            <h3 class="burger-title">Liste des tableaux :</h3>
-                            <ul>
-                                <li class="table-brush burger-list">Tableau de testeur1 <i class="fas fa-paint-brush"></i></li>
-                                <li class="burger-list board-2" >Tableau 2 de testeur1</li>
-                                <li class="burger-list board-3" >Tableau 3 de testeur1</li>
-                                <li class="burger-list board-4" >Tableau 4 de testeur1</li>
-                                <li class="burger-add-table">Ajouter un tableau</li>
-                            </ul>
-                        <div class="modify-table">
-                            <h4 class="burger-liste"> Modifier le tableau actif : </h4>
-                                <form action="">
-                                    <input type="text" class="input" value="" placeholder="Titre du tableau" name="Titre du tableau">
-                                    <input class="colorpicker" type="color" value="#FBFBF9" placeholder="Choisir une couleur" name="Choisir une couleur">
-                                    <input type="text" class="input" value="" placeholder="Choisir une image de fond" name="Choisir une image de fond">
-                                    <div class="edit-table-image">
-                                            <a class="table-thumbs" href="https://placeholder.com"><img src="https://via.placeholder.com/55" alt=""></a>
-                                            <a class="table-thumbs" href="https://placeholder.com"><img src="https://via.placeholder.com/55" alt=""></a>
-                                            <a class="table-thumbs" href="https://placeholder.com"><img src="https://via.placeholder.com/55" alt=""></a>
-                                            <a class="table-thumbs" href="https://placeholder.com"><img src="https://via.placeholder.com/55" alt=""></a>
-                                            <a class="table-thumbs" href="https://placeholder.com"><img src="https://via.placeholder.com/55" alt=""></a>
-                                            <a class="table-thumbs" href="https://placeholder.com"><img src="https://via.placeholder.com/55" alt=""></a>
-                                            <a class="table-thumbs" href="https://placeholder.com"><img src="https://via.placeholder.com/55" alt=""></a>
-                                            <a class="table-thumbs" href="https://placeholder.com"><img src="https://via.placeholder.com/55" alt=""></a>
-                                    </div>
-                            </form>
-                        </div>
+    <div id="burger-wrap" class="burger-header">
+        <div class="btn-burger">
+            <i id="burger-open"class="fas fa-arrow-right"></i>
+        
+            <i id="burger-close" class="fas fa-arrow-left"></i>
+        </div>
+        
+        <nav class="burger-nav">
+            <h3 class="burger-title">Liste des tableaux :</h3>
+            <ul>
+                <li class="table-brush burger-list">Tableau de testeur1 <i class="fas fa-paint-brush"></i></li>
+                <li class="burger-list board-2" >Tableau 2 de testeur1</li>
+                <li class="burger-list board-3" >Tableau 3 de testeur1</li>
+                <li class="burger-list board-4" >Tableau 4 de testeur1</li>
+                <li class="burger-add-table">Ajouter un tableau</li>
+            </ul>
 
-                        <div class="burger-new-table">
-                            <h4 class="burger-liste">Nouveau tableau : </h4>
-                                <form action="">
-                                        <input type="text" class="input" value="" placeholder="Titre du tableau" name="Titre du tableau">
-                                        <input class="colorpicker" type="color" value="#FBFBF9" placeholder="Choisir une couleur" name="Choisir une couleur">
-                                        <input type="text" class="input" value="" placeholder="Choisir une image de fond" name="Choisir une image de fond">
-                                </form>
-                        </div>
-
-                     </nav>
-                </header>
+            <div class="modify-table">
+                <h4 class="burger-liste">Modifier le tableau actif : </h4>
+                <form action="">
+                    <label class="label-modify-table" for="name">Titre du tableau</label><br>
+                        <input type="text" class="input" value="" placeholder="" name="Titre du tableau"> <br>
+                    <label class="label-modify-table" for="name">Choisir une couleur</label><br>
+                        <input class="colorpicker" type="color" value="#FBFBF9" placeholder="" name="Choisir une couleur"> <br>
+                    <label class="label-modify-table" for="name">Choisir une image de fond</label><br>
+                        <input type="text" class="input" value="" placeholder="" name="Choisir une image de fond"> 
+                    <div class="edit-table-image">
+                        <a class="table-thumbs" href="https://placeholder.com"><img src="https://via.placeholder.com/55" alt=""></a>
+                        <a class="table-thumbs" href="https://placeholder.com"><img src="https://via.placeholder.com/55" alt=""></a>
+                        <a class="table-thumbs" href="https://placeholder.com"><img src="https://via.placeholder.com/55" alt=""></a>
+                        <a class="table-thumbs" href="https://placeholder.com"><img src="https://via.placeholder.com/55" alt=""></a>
+                        <a class="table-thumbs" href="https://placeholder.com"><img src="https://via.placeholder.com/55" alt=""></a>
+                        <!-- <a class="table-thumbs" href="https://placeholder.com"><img src="https://via.placeholder.com/55" alt=""></a>
+                        <a class="table-thumbs" href="https://placeholder.com"><img src="https://via.placeholder.com/55" alt=""></a>
+                        <a class="table-thumbs" href="https://placeholder.com"><img src="https://via.placeholder.com/55" alt=""></a> -->
+                    </div>
+                </form>
             </div>
+
+            <div class="burger-new-table">
+                <h4 class="burger-liste">Nouveau tableau : </h4>
+                <form action="">
+                    <label class="label-modify-table" for="name">Titre du tableau</label><br>
+                        <input type="text" class="input" value="" placeholder="" name="Titre du tableau"> <br>
+                    <label class="label-modify-table" for="name">Choisir une couleur</label><br>
+                        <input class="colorpicker" type="color" value="#FBFBF9" placeholder="" name=""> <br>
+                    <label class="label-modify-table" for="name">Choisir une image de fond</label><br>
+                        <input type="text" class="input" value="" placeholder="" name="Choisir une image de fond"><br>
+                </form>
+            </div>
+        </nav>
+    </div>
 </div>
--- app/js/script.js
+++ app/js/script.js
@@ -1,24 +1,53 @@
-//On click on the burger icon, hide it
-$('#burger-open').on('click', function() {
-    $('#burger-open').hide();
-    //On click on the burger icon, hide ModifyTable
-    $('.modify-table').hide();
-    //On click on the burger icon, hide NewBurgerTable
-    $('.burger-new-table').hide();
-});
+// //On click on the burger icon, hide it
+// $('#burger-open').on('click', function() {
+//     $('#burger-open').hide();
+//     //On click on the burger icon, hide ModifyTable
+//     $('.modify-table').hide();
+//     //On click on the burger icon, hide NewBurgerTable
+//     $('.burger-new-table').hide();
+// });
 
-//On click on the X close, show the burger icon
-$('#close').on('click', function() {
-    $('#burger-open').show();
-});
+// //On click on the X close, show the burger icon
+// $('#close').on('click', function() {
+//     $('#burger-open').show();
+//     $('#burger-open').shide();
+// });
 
-// On click brush show ModifyTable
-$('.fa-paint-brush').on('click', function() {
-    $('.modify-table').show();
-});
+// // On click brush show ModifyTable
+// $('.fa-paint-brush').on('click', function() {
+//    $('.modify-table').hide();
+//    $('.modify-table').show();
+// });
 
-// On click burger-add-table show NewBurgerTable
-$('.burger-add-table').on('click', function() {
-    $('.burger-new-table').show();
-});
+// // On click burger-add-table show NewBurgerTable
+// $('.burger-add-table').on('click', function() {
+//     $('.burger-new-table').show();
+//     $('.burger-new-table').hide();
+// });
 
+// Menu
+/* Set the width of the side navigation to 310px */
+function openNav() {
+    document.getElementByClass(".burger-header").style.width = "310px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementByClass(".burger-header").style.width = "50px";
+  }
+
+  $('#burger-open').on('click', function() {
+    console.log("opening")
+    document.querySelector(".burger-header").style.width = "310px";
+    document.querySelector("#burger-open").style.display = "none";
+    document.querySelector("#burger-close").style.display = "block";
+    document.querySelector(".burger-nav").style.display = "block";
+  });
+
+  $('#burger-close').on('click', function() {
+    console.log("closing")
+    document.querySelector(".burger-header").style.width = "50px";
+    document.querySelector("#burger-close").style.display = "none";
+    document.querySelector("#burger-open").style.display = "block";
+    document.querySelector(".burger-nav").style.display = "none";
+});
