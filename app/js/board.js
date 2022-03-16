bbd2855837fbfa6c53699990b4dbe976fddc5cfb
ninaSdev <ninasitdev11@gmail.com>
2022-03-16 18:10:55 +0100

    modifButtons

--- app/js/board.js
+++ app/js/board.js
@@ -1,6 +1,6 @@
 let app = {
     // Penser à modifier ici l'adresse de votre API
-    baseUrl: 'http://localhost/cda/Projets/cda_projet_project_hive/app/?api/',
+    baseUrl: 'http://localhost/cda_projet_project_hive/app/?api/',
     
     maxListeOrderNb: 0,
     loadedBoard: {
